3663a2521db2ebd070f0847b1c572444aa1ec5f1
Leer <85291330+TheLeerName@users.noreply.github.com>
2026-06-29 12:35:28 +0700

    v1.0.2 - fix of already broken worlds part 2

--- src/main/java/com/theleername/lithostitchedleveldatdepthfixer/mixin/LevelStorageSourceMixin.java
+++ src/main/java/com/theleername/lithostitchedleveldatdepthfixer/mixin/LevelStorageSourceMixin.java
@@ -3,27 +3,23 @@ package com.theleername.lithostitchedleveldatdepthfixer.mixin;
 import com.theleername.lithostitchedleveldatdepthfixer.Main;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtAccounter;
+import net.minecraft.nbt.NbtIo;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 @Mixin(LevelStorageSource.class)
 public class LevelStorageSourceMixin {
-	@Inject(method = "readLevelDataTagRaw", at = @At("HEAD"))
-	private static void worldfix$repair(Path levelPath,CallbackInfoReturnable<CompoundTag> cir) {
-		Main.fix(levelPath);
-	}
-
-	@ModifyArg(method = "readLevelDataTagRaw", at = @At(
+	@Redirect(method = "readLevelDataTagRaw", at = @At(
 		value = "INVOKE",
 		target = "Lnet/minecraft/nbt/NbtIo;readCompressed(Ljava/nio/file/Path;Lnet/minecraft/nbt/NbtAccounter;)Lnet/minecraft/nbt/CompoundTag;"
-	), index = 1)
-	private static NbtAccounter worldfix$depth(NbtAccounter original) {
-		return new NbtAccounter(104857600L, Main.MAX_DEPTH);
+	))
+	private static CompoundTag worldfix$read(Path path, NbtAccounter original) throws IOException {
+		Main.fix(path);
+		return NbtIo.readCompressed(path, new NbtAccounter(104857600L, 512));
 	}
 }
